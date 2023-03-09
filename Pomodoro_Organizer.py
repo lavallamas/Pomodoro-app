8b8b2abccd54b8e78f4382b850fe5d2e2c7f83b5
lavallamas <122215850+lavallamas@users.noreply.github.com>
2023-03-09 16:29:08 -0500

    Added placeholder text and reorged functions, debugged options menu, added list function

--- Pomodoro_Organizer.py
+++ Pomodoro_Organizer.py
@@ -1,23 +1,34 @@
-
-def main():
-    print("Welcome to the Pomodoro Organizer!")
-    options()
-main()
+def timer():
+    print("timer")
+def task_list():
+    tasks = [1, 2, 3]
+    print("To-Do", *tasks, sep="\n")
+    
+def daily_schedule():
+    print("placeholder")
+def weekly_schedule():
+    print("placeholder")
 def options():
-    input('''How can I help you today? 
+    user_input = input('''How can I help you today? 
     1.Pomodoro Timer
     2.Task List
     3.Daily Schedule
     4.Week Schedule
     ''')
-    if input == 1:
+    if user_input == "1":
         timer()
-    elif input == 2:
-        task()
-    elif input == 3:
+    elif user_input == "2":
+        task_list()
+    elif user_input == "3":
         daily()
-    elif input == 4:
+    elif user_input == "4":
         week()
     else:
         print("invalid input try again")
         options()
+
+
+def main():
+    print("Welcome to the Pomodoro Organizer!")
+    options()
+main()
